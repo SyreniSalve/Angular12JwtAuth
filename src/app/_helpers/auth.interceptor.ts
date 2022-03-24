4d95bd01f4cbd16002ed7ab3d630a3f3cfac6135
Simona Liamo <liamosimona@yahoo.com>
2022-03-24 14:17:08 +0200

    add Angular Jwt authentication example

---
+++ src/app/_helpers/auth.interceptor.ts
@@ -0,0 +1,71 @@
+import { HTTP_INTERCEPTORS, HttpEvent, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpHandler, HttpRequest } from "@angular/common/http";
+import { TokenStorageService } from "../_services/token-storage.service";
+import { AuthService } from "../_services/auth.service";
+import { BehaviorSubject, Observable, throwError } from "rxjs"
+import { catchError, filter, switchMap, take } from "rxjs";
+
+const TOKEN_HEADER_KEY = 'Authorization'; // for spring-boot back-end
+
+@Injectable()
+
+export class AuthInterceptor implements HttpInterceptor {
+
+  private isRefreshing: boolean = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
+  constructor(private token: TokenStorageService, private authService: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<Object>> {
+    let authReq = req;
+    const token = this.token.getToken();
+    if (token != null) {
+      authReq = this.addTokenHeader(req, token);
+    }
+    // @ts-ignore
+    return next.handle(authReq).pipe(catchError (error => {
+      if (error instanceof HttpErrorResponse && !authReq.url.includes('auth/signin') && error.status === 401) {
+        return this.handle401Error(authReq, next);
+      }
+      return throwError(error);
+    }));
+  }
+  // @ts-ignore
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
+      const token = this.token.getRefreshToken();
+      if (token){
+        return this.authService.refreshToken(token).pipe(
+          switchMap((token: any) => {
+            this.isRefreshing = false;
+            this.token.saveToken(token.accessToken);
+            this.refreshTokenSubject.next(token.accessToken);
+
+            return next.handle(this.addTokenHeader(request, token.accessToken))
+          }),
+          catchError((err) => {
+            this.isRefreshing = false;
+            this.token.signOut();
+            return throwError(err);
+          })
+        );
+      }
+      return this.refreshTokenSubject.pipe(
+        filter(token => token !== null),
+        take(1),
+        switchMap((token) => next.handle(this.addTokenHeader(request, token)))
+      );
+    }
+  }
+
+  private addTokenHeader(request: HttpRequest<any>, token: string) {
+    return request.clone({headers: request.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+  }
+}
+
+export const authInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+];
